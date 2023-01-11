f97a56fdd4634881306f1ac6f6d4e98f91d037aa
MU <matthias.ulpmer@brandnamic.com>
2023-01-11 09:13:01 +0100

    task (admin): add files

---
+++ Packages/Build/source/js/entries/sections/admin.js
@@ -0,0 +1,22 @@
+/*  ==========================================================================
+    HERO
+    ========================================================================== */
+
+// node modules imports
+import $ from 'cash-dom';
+
+// local imports
+import * as vars from 'tk-source-root/js/variables/variables';
+import { inView } from 'tk-source-root/js/utilities/in-view';
+
+/* CODE
+ * --------------------------------------------------------------------------- */
+
+// define identifier
+const identifier = 'admin';
+
+// initialize module
+const $section = $(`.${vars.CLASSNAMES.sect}[data-id="${identifier}"]`);
+if ($section.length) {
+
+}
---
+++ Packages/Resources/Private/Sections/admin.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="de">
+<section id="EBIL-section--admin" class="EBIL-section" data-id="admin" data-hero-layout="full">
+    <div class="EBIL-section__wrap">
+        <?php
+
+        ?>
+    </div>
+</section>
+</html>
+
