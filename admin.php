3b3f0f49d484ba45f0df2f1f1fbd9eeeeccc5d1c
Ivan Bacher <stbaciva@bx.fallmerayer.it>
2023-01-11 15:46:28 +0100

    task (admin): menu bar

--- admin.php
+++ admin.php
@@ -1,10 +1,41 @@
 <!DOCTYPE html>
 <html lang="de">
+<style>
+    ul {
+        list-style-type: none;
+        margin: 0;
+        padding: 0px;
+        overflow: hidden;
+        background-color: black;
+    }
+
+    li {
+        float: left;
+    }
+
+    li a {
+        display: block;
+        color: blue;
+        font-size:20px;
+        text-align: center;
+        padding: 10px 20px;
+        text-decoration: none;
+    }
+    .active{
+        background-color: gray;
+        color: white;
+    }
+    li a:hover {
+        background-color: green;
+        color: white;
+    }
+</style>
 <section id="EBIL-section--admin" class="EBIL-section" data-id="admin" data-hero-layout="full">
     <div class="EBIL-section__wrap">
-        <?php
-            echo "Ulpmer ist schleccht!";
-        ?>
+        <ul>
+            <li><a class="active" href="#home">Tagebuch</a></li>
+            <li><a href="#">Auto</a></li>
+        </ul>
     </div>
 </section>
 </html>
