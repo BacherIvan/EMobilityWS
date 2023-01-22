5cf1e8d65424e50343e38951766584ff20110e28
MU <matthias.ulpmer@brandnamic.com>
2023-01-22 15:09:33 +0100

    task (admin): add menu and style entries

--- Packages/Resources/Public/JavaScript/entry--sections--admin.js
+++ Packages/Resources/Public/JavaScript/entry--sections--admin.js
@@ -31,16 +31,86 @@ const identifier = 'admin';
 // initialize module
 const $section = cash_dom__WEBPACK_IMPORTED_MODULE_0___default()(`.${tk_source_root_js_variables_variables__WEBPACK_IMPORTED_MODULE_1__["CLASSNAMES"].sect}[data-id="${identifier}"]`);
 if ($section.length) {
-  console.log("HAAAAALO");
+  // Tagebuch-Einträge
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
-      var values = JSON.parse(xhttp.responseText);
-      console.log(values);
+      var rows = JSON.parse(xhttp.responseText);
+      for (const item of rows) {
+        const span = document.getElementById('JS-eintrag');
+        var name = document.createTextNode(item['vorname'] + ' ' + item['nachname']);
+        span.appendChild(name);
+        var linebreak = document.createElement('br');
+        span.appendChild(linebreak);
+        var time = document.createTextNode(item['startzeit'] + ' - ' + item['entzeit']);
+        span.appendChild(time);
+        linebreak = document.createElement('br');
+        span.appendChild(linebreak);
+        var entry = document.createTextNode(item['eintrag']);
+        span.appendChild(entry);
+        linebreak = document.createElement('br');
+        span.appendChild(linebreak);
+      }
     }
   };
-  xhttp.open("GET", "/config/db/Tagebuch.php?datum='2022-11-21'");
+  xhttp.open("GET", "/config/db/Tagebuch.php?datum=" + "2022-11-21");
   xhttp.send(null);
+
+  // Personen für Drop-Down Menü
+  class Person {
+    constructor(IDP, vorname, nachname) {
+      this.IDP = IDP;
+      this.vorname = vorname;
+      this.nachname = nachname;
+    }
+  }
+  class PersonList {
+    constructor() {
+      this.list = [];
+    }
+    addPerson(person) {
+      this.list.push(person);
+    }
+    getPerson(index) {
+      return this.list[index];
+    }
+    getPersonList() {
+      return this.list;
+    }
+  }
+  let personList = new PersonList();
+  var xhttpPersons = new XMLHttpRequest();
+  xhttpPersons.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      var rows = JSON.parse(xhttpPersons.responseText);
+      for (const item of rows) {
+        let person = new Person(item["IDP"], item["vorname"], item["nachname"]);
+        personList.addPerson(person);
+      }
+      console.log(personList.getPersonList());
+    }
+  };
+  xhttpPersons.open("GET", "/config/db/Persons.php");
+  xhttpPersons.send(null);
+
+  // Auf den Menüpunkt, der gecklickt wurde, die Klasse 'JS-act' setzen
+  $section.find('.JS-anchor').on('click', function () {
+    $section.find('.JS-act').removeClass('JS-act');
+    cash_dom__WEBPACK_IMPORTED_MODULE_0___default()(this).addClass('JS-act');
+    const target = cash_dom__WEBPACK_IMPORTED_MODULE_0___default()(this).attr('data-target'),
+      $element = cash_dom__WEBPACK_IMPORTED_MODULE_0___default()(`#${target}`);
+    if (target == 'c51') {
+      var remove_element = document.getElementById('c50');
+      var add_element = document.getElementById('c51');
+      remove_element.classList.remove('JS-act');
+      add_element.classList.add('JS-act');
+    } else {
+      var remove_element = document.getElementById('c51');
+      var add_element = document.getElementById('c50');
+      remove_element.classList.remove('JS-act');
+      add_element.classList.add('JS-act');
+    }
+  });
 }
 
 /***/ })
--- admin.php
+++ admin.php
@@ -7,119 +7,65 @@
 <!DOCTYPE html>
 <html lang="de">
 <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, user-scalable=yes, maximum-scale=1, minimum-scale=1, viewport-fit=cover">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="theme-color" content="#ffffff">
     <title>Admin</title>
-    <style>
-        ul {
-            list-style-type: none;
-            margin: 0;
-            padding: 0px;
-            overflow: hidden;
-            background-color: black;
-        }
-
-        li {
-            float: left;
-        }
-
-        li a {
-            display: block;
-            color: blue;
-            font-size:20px;
-            text-align: center;
-            padding: 10px 20px;
-            text-decoration: none;
-        }
-        .active{
-            background-color: gray;
-            color: white;
-        }
-        li a:hover {
-            background-color: green;
-            color: white;
-        }
-    </style>
-    <script>
-        var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                var rows = JSON.parse(xhttp.responseText);
-                for (const item of rows) {
-                    const span = document.getElementById('JS-eintrag');
-
-                    var name = document.createTextNode(item['vorname'] + ' ' + item['nachname']);
-                    span.appendChild(name);
-                    var linebreak = document.createElement('br');
-                    span.appendChild(linebreak);
-
-                    var time = document.createTextNode(item['startzeit'] + ' - ' + item['entzeit']);
-                    span.appendChild(time);
-                    linebreak = document.createElement('br');
-                    span.appendChild(linebreak);
-
-                    var entry = document.createTextNode(item['eintrag']);
-                    span.appendChild(entry);
-                    linebreak = document.createElement('br');
-                    span.appendChild(linebreak);
-                }
-            }
-        };
-        xhttp.open("GET", "/config/db/Tagebuch.php?datum=" + "2022-11-21");
-        xhttp.send(null);
-    </script>
-    <script>
-        class Person {
-            constructor(IDP, vorname, nachname) {
-                this.IDP = IDP;
-                this.vorname = vorname;
-                this.nachname = nachname;
-            }
-        }
-        class PersonList {
-            constructor() {
-                this.list = [];
-            }
-
-            addPerson(person) {
-                this.list.push(person);
-            }
-
-            getPerson(index) {
-                return this.list[index];
-            }
-
-            getPersonList() {
-                return this.list;
-            }
-        }
-
-        let personList = new PersonList();
-        var xhttpPersons = new XMLHttpRequest();
-        xhttpPersons.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                var rows = JSON.parse(xhttpPersons.responseText);
-
-                for(const item of rows){
-                    let person = new Person(item["IDP"], item["vorname"], item["nachname"]);
-                    personList.addPerson(person);
-                }
-                console.log(personList.getPersonList());
-            }
-        };
-        xhttpPersons.open("GET", "/config/db/Persons.php");
-        xhttpPersons.send(null);
-    </script>
+    <link media="screen" rel="stylesheet" type="text/css" href="/Packages/Resources/Public/Css/Frontend/sections--admin.css">
+    <script type="text/javascript" async src="/Packages/Resources/Public/JavaScript/entry--globals--global.js"></script>
+    <script type="text/javascript" async src="/Packages/Resources/Public/JavaScript/entry--sections--admin.js"></script>
+    <script type="text/javascript" async src="/Packages/Resources/Public/JavaScript/tk-internal-functions.js"></script>
+    <script type="text/javascript" async src="/Packages/Resources/Public/JavaScript/vendor--cash-dom.js"></script>
+    <script type="text/javascript" async src="/Packages/Resources/Public/JavaScript/vendor--lazysizes.js"></script>
+    <script type="text/javascript" async src="/Packages/Resources/Public/JavaScript/vendor--progressbar-js.js"></script>
+    <script type="text/javascript" async src="/Packages/Resources/Public/JavaScript/vendor--lodash-es.js"></script>
+    <script type="text/javascript" async src="/Packages/Resources/Public/JavaScript/vendor--shifty.js"></script>
+    <script type="text/javascript" async src="/Packages/Resources/Public/JavaScript/webpack--runtime.js"></script>
     <link rel="icon" type="image/png" sizes="16x16" href="/Packages/Resources/Public/Images/logo/favicon.png">
 </head>
 
 <section id="EBIL-section--admin" class="EBIL-section" data-id="admin" data-hero-layout="full">
     <div class="EBIL-section__wrap">
-        <ul>
-            <li><a class="active" href="#home">Tagebuch</a></li>
-            <li><a href="#auto">Auto</a></li>
-        </ul>
-        <div id="JS-eintrag"></div>
+        <div class="EBIL-section__admin-menu">
+            <ul class="EBIL-section__admin-menu__lvl EBIL-section__admin-menu__lvl--0">
+                <li class="EBIL-section__admin-menu__item EBIL-section__admin-menu__item--lvl-0 EBIL-section__admin-menu__item--first JS-anchor">
+                    <a href="/" class="EBIL-section__admin-menu__link EBIL-section__admin-menu__link--lvl-0 " target="_top" title="Home">
+                        <div class="EBIL-section__admin-menu__number">
+                            <span class=""> 01 </span>
+                        </div>
+                        <span class="EBIL-section__admin-menu__title EBIL-section__admin-menu__title--lvl-0"> Home </span>
+                    </a>
+                </li>
+                <li class="EBIL-section__admin-menu__item EBIL-section__admin-menu__item--lvl-0 JS-anchor JS-act" data-target="c50">
+                    <a class="EBIL-section__admin-menu__link EBIL-section__admin-menu__link--lvl-0" target="_top" title="Tabebuch">
+                        <div class="EBIL-section__admin-menu__number">
+                            <span class=""> 02 </span>
+                        </div>
+                        <span class="EBIL-section__admin-menu__title EBIL-section__admin-menu__title--lvl-0"> Tagebuch </span>
+                    </a>
+                </li>
+                <li class="EBIL-section__admin-menu__item EBIL-section__admin-menu__item--lvl-0 JS-anchor" data-target="c51">
+                    <a class="EBIL-section__admin-menu__link EBIL-section__admin-menu__link--lvl-0 " target="_top" title="Auto">
+                        <div class="EBIL-section__admin-menu__number">
+                            <span class=""> 03 </span>
+                        </div>
+                        <span class="EBIL-section__admin-menu__title EBIL-section__admin-menu__title--lvl-0"> Auto </span>
+                    </a>
+                </li>
+        </div>
+        <div class="EBIL-section__tagebuch JS-act" id="c50">
+            <div class="EBIL-section__neuer-eintrag">
+                <span>Neuer Eintrag</span>
+            </div>
+            <div class="EBIL-section__eintrag-wrap">
+                <div id="JS-eintrag"></div>
+            </div>
+        </div>
+        <div class="EBIL-section__auto" id="c51">
+            <span>AUTOOO....FOCK DU</span>
+        </div>
     </div>
-
 </section>
 </html>
 
